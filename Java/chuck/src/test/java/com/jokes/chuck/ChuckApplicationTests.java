38b8bd63e8e84fb5da57aa2228e9e5250c990ddd
Erin Cullison <cullison.erin@gmail.com>
2023-02-23 14:36:13 -0800

    Added Spring project

---
+++ Java/chuck/src/test/java/com/jokes/chuck/ChuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokes.chuck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
