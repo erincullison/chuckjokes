a362436153cf5b4d8fd1d3d5b72d018c739a8c06
paigemcca <paigemcca@gmail.com>
2023-02-24 13:50:32 -0800

    class setup

---
+++ chuck/java/src/main/java/com/techelevator/model/Jokes.java
@@ -0,0 +1,50 @@
+package com.techelevator.model;
+
+public class Jokes {
+
+    private String iconUrl;
+    private String id;
+    private String url;
+    private String value;
+    private String category;
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
