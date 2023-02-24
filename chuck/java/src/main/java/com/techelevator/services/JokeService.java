62838a4d91e7daf43e2001200fd5e4e3024ead40
paigemcca <paigemcca@gmail.com>
2023-02-24 15:44:30 -0800

    api variables

--- chuck/java/src/main/java/com/techelevator/services/JokeService.java
+++ chuck/java/src/main/java/com/techelevator/services/JokeService.java
@@ -1,4 +1,16 @@
 package com.techelevator.services;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class JokeService {
+
+    @Value("${JOKES_API}")
+    private String JOKES_API;
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    
 }
