dec07f3d3a954c0379cfc59eef9c2c8f10366ad9
paigemcca <paigemcca@gmail.com>
2023-02-24 16:27:27 -0800

    controller test

--- chuck/java/src/main/java/com/techelevator/controller/JokeController.java
+++ chuck/java/src/main/java/com/techelevator/controller/JokeController.java
@@ -1,4 +1,19 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.Joke;
+import com.techelevator.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
 public class JokeController {
+
+    @Autowired
+    JokeService service;
+
+    @GetMapping("/joke")
+    public Joke getJokeFromApi(){return service.getJoke();}
 }
